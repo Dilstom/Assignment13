09b10ce9d974b5044600754d846e84d465c216e7
Dil Somb <dils@Dils-MacBook-Pro.local>
2021-05-18 11:52:10 -0700

    clean code

--- bankaccount/js/script.js
+++ bankaccount/js/script.js
@@ -2,79 +2,90 @@ const $ = (id) => document.getElementById(id);
 
 // VARIABLES
 
-let btnOwnerName = $('ownerName');
-let btnDeposit = $('deposit');
-let btnWithdrawal = $('withdrawal');
-let divDisplayInfo = $('displayInfo');
+let btnOwnerName = $("ownerName");
+let btnDeposit = $("deposit");
+let btnWithdrawal = $("withdrawal");
+let divDisplayInfo = $("displayInfo");
 
-let balance = 0;
-let owner;
+let bankAccount = function () {
 
-function bankAccount (ownerName) {
-    // LOCAL MEMBERS
-        owner = ownerName;
+  // LOCAL MEMBERS
+  let balance = 0;
+  let owner = "";
 
-        let numRegex = /^[+]?([0-9]+(?:[\.][0-9]*)?|\.[0-9]+)$/;
-    
-    // PUBLIC MEMBERS
-    return  {
-        withdrawalAmount: function(amount) {
-            if(numRegex.test(amount)){
-                if(balance >= amount) {
-                    balance -= Number(amount);
-                    this.getBalance(balance);
-                }else {
-                    alert('Error - you do not have sufficient funds to withdraw this amount')
-                    this.getBalance();
+  // REGEX TEST
+  let numRegex = /^[+]?([0-9]+(?:[\.][0-9]*)?|\.[0-9]+)$/;
+
+  return {
+    getBalance: function () {
+    //   console.log("balance: ", balance);
+    //   console.log("owner: ", owner);
+    //   console.log("this in get: ", this); // OBJECT
+    //   console.log("self: ", self); // WINDOW
+      if (owner) {
+        divDisplayInfo.innerHTML = `Welcome ${owner} <br>Your balance is: ${balance}`;
+      } else {
+        divDisplayInfo.innerHTML = `Please, enter your name first`;
+      }
+    },
+    withdrawalAmount: function () {
+        var self = this;
+        return function() { // TO USE 'THIS' 
+            if (owner) {
+              let amount = prompt("How much do you want to withdraw?");
+
+              if (numRegex.test(amount)) {
+                if (balance >= amount) {
+                  balance -= Number(amount);
+                  self.getBalance(balance);
+                } else {
+                  alert(
+                    "Error - you do not have sufficient funds to withdraw this amount"
+                  );
+                  self.getBalance();
                 }
-            }else {
-                alert('Please, enter a valid number');
-            }
-        },
-        depositAmount: function(amount) {
-            // let amount = prompt('How much do you want to deposit?')
-            console.log('test: ', );
-            if(numRegex.test(amount)){
-                balance += Number(amount);
-                this.getBalance();
+              } else {
+                alert("Please, enter a valid number");
+              }
             } else {
-                alert('Please, enter a valid number');
-            }
-            
-            // console.log('balanc')
-        },
-        getBalance: function() {
-            console.log('balance: ', balance);
-            if(owner){
-                divDisplayInfo.innerHTML = `Welcome ${owner} <br>Your balance is: ${balance}`
-                return  balance;
-            }
-        },
-        getOwnerName: function() {
-            // owner = prompt('Your name: ');
-            console.log('name: ', owner);
-            if(owner){
-                return owner;
+              alert("Please, provide your name");
             }
         }
-    }
+    },
+    depositAmount: function () {
+        let self = this;
+        return function () { // THE ONLY WAY TO GET 'THIS' KEYWORD IS TO RETURN WHOLE FUNCTION
+            if (owner) {
+              let amount = prompt("How much do you want to deposit?");
+            //   console.log("this: ", this); // BUTTON
+            //   console.log("self: ", self); // OBJECT WITH METHODS
+      
+              if (numRegex.test(amount)) {
+                balance += Number(amount);
+                // console.log("balance deposit: ", balance);
+                self.getBalance();
+              } else {
+                alert("Please, enter a valid number");
+              }
+            } else {
+              alert("Please, provide your name");
+            }
+        }
+    },
+    getOwnerName: function () {
+      owner = prompt("Your name: ");
+      if (owner) {
+        divDisplayInfo.innerHTML = `Welcome ${owner} <br>Your balance is: ${balance}`;
+        return owner;
+      }
+    },
+  };
 };
 
-btnOwnerName.addEventListener('click', function() {
-    owner = prompt('Your name: ');
-    bankAccount(owner).getOwnerName();
-});
+let account1 = bankAccount(); // STORE THE FUNCTION TO ACCOUNT1 VARIABLE - NOW WE CAN PERSIST VARIABLES
 
-btnDeposit.addEventListener('click', function() {
-    let amount = prompt('How much do you want to deposit?')
-    bankAccount(owner).depositAmount(amount);
-    // bankAccount('Alex').depositAmount(amount);
-});
+btnOwnerName.onclick = account1.getOwnerName;
 
-btnWithdrawal.addEventListener('click', function() {
-    let amount = prompt('How much do you want to withdraw?')
-    bankAccount(owner).withdrawalAmount(amount);
-});
+btnDeposit.onclick =  account1.depositAmount(); // SINCE WE RETURN THE FUNCTION,s USE ()
 
-
-// console.log(bankAccount(owner).getBalance());
+btnWithdrawal.addEventListener("click", account1.withdrawalAmount()); // THE SAME AS ABOVE
